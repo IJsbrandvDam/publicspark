7aaeff620f83c535a59b9555da1a50d9b4d96784
IJsbrandvDam <ivandam@cisco.com>
2017-05-04 13:51:59 +0200

    Update DBconnect.py

--- DBconnect.py
+++ DBconnect.py
@@ -1,5 +1,4 @@
 import mysql.connector
-import string
 
 def createDatabase(personName):
 	conn = mysql.connector.connect(user='brainspark', password='C!sco123',
@@ -68,23 +67,23 @@ def pullFromDatabase():
 
 
 def createTemplate(tempName):
-	s = stripWhiteSpace(tempName)
+	#s = stripWhiteSpace(tempName)
 	conn = mysql.connector.connect(user='brainspark', password='C!sco123',
                               host='brainspark.cptvcix7ijfy.us-west-2.rds.amazonaws.com',
                               database='brainspark')
 	mycursor=conn.cursor()
-	mycursor.execute("CREATE TABLE '%s' (Question INT PRIMARY KEY AUTO_INCREMENT, Answer TEXT)" % (s))
+	mycursor.execute("CREATE TABLE (?) (Question INT PRIMARY KEY AUTO_INCREMENT, Answer TEXT)" % (tempName))
 	mycursor.execute("INSERT INTO Templates (Question) VALUES ('%s')" % (tempName))
 	conn.commit()
 	return "true"
 
 def sendToTemp(tempName,question):
-	s = stripWhiteSpace(tempName)
+	#s = stripWhiteSpace(tempName)
 	conn = mysql.connector.connect(user='brainspark', password='C!sco123',
                               host='brainspark.cptvcix7ijfy.us-west-2.rds.amazonaws.com',
                               database='brainspark')
 	mycursor=conn.cursor()
-	mycursor.execute("INSERT INTO %s (Answer) VALUES ('%s')" % (s,question))
+	mycursor.execute("INSERT INTO (?) (Answer) VALUES ('%s')" % (tempName,question))
 	conn.commit()
 	return "true"
 
@@ -92,7 +91,7 @@ def stripWhiteSpace(stringText):
 	print("begonnen met strippen van: " + stringText)
 	s = stringText
 	whitespace = " "
-	for whitespace in string.s:
+	for whitespace in s:
 		s.replace(" ", "")
 
 	print("klaar, eindresultaat is: " + s)
