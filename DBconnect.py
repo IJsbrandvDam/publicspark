34220064e60a59562a99099ea6708e089557bbec
IJsbrandvDam <ivandam@cisco.com>
2017-05-04 13:43:18 +0200

    Update DBconnect.py

--- DBconnect.py
+++ DBconnect.py
@@ -67,31 +67,32 @@ def pullFromDatabase():
 
 
 def createTemplate(tempName):
+	s = stripWhiteSpace(tempName)
 	conn = mysql.connector.connect(user='brainspark', password='C!sco123',
                               host='brainspark.cptvcix7ijfy.us-west-2.rds.amazonaws.com',
                               database='brainspark')
 	mycursor=conn.cursor()
-	#tempName2 = tempName.replace(" ","")
-	mycursor.execute("CREATE TABLE '%s' (Question INT PRIMARY KEY AUTO_INCREMENT, Answer TEXT)" % (tempName))
+	mycursor.execute("CREATE TABLE '%s' (Question INT PRIMARY KEY AUTO_INCREMENT, Answer TEXT)" % (s))
 	mycursor.execute("INSERT INTO Templates (Question) VALUES ('%s')" % (tempName))
 	conn.commit()
 	return "true"
 
 def sendToTemp(tempName,question):
-	#tempName2 = tempName.replace(" ","")
+	s = stripWhiteSpace(tempName)
 	conn = mysql.connector.connect(user='brainspark', password='C!sco123',
                               host='brainspark.cptvcix7ijfy.us-west-2.rds.amazonaws.com',
                               database='brainspark')
 	mycursor=conn.cursor()
-	mycursor.execute("INSERT INTO %s (Answer) VALUES ('%s')" % (tempName,question))
+	mycursor.execute("INSERT INTO %s (Answer) VALUES ('%s')" % (s,question))
 	conn.commit()
 	return "true"
 
+def stripWhiteSpace(string):
+	s = string
+	for whitespace in s:
+		s.replace(" ", "")
 
-
-
-
-
+	return s
 
 
 # createDatabase("chris")
